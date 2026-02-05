678a5906334fc94252785edebbd3acc79eb25d9e
nahilrasheed <88521572+nahilrasheed@users.noreply.github.com>
2026-02-06 00:46:07 +0530

    feat: update site configuration and enhance index page layout

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,7 +5,7 @@ export const theme: ThemeUserConfig = {
   /** Title for your website. Will be used in metadata and as browser tab title. */
   title: '0xnhl',
   /** Will be used in index page & copyright declaration */
-  author: 'Nahil',
+  author: 'Nahil Rasheed',
   /** Description metadata for your website. Can be used in page metadata. */
   description: 'Website',
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
